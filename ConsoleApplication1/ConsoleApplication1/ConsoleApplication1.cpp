b4c33be392d3abfca7a146fd55b06e8e73d1b7bd
gitFedos <yegorf2010@mail.ru>
2016-04-08 09:35:33 +0500

    start cpp hello feature

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	std::cout << "Hello worldik";
+	return 0;
+}
+
