993649a17c4c0432b4a45dccb17a2acca4ad0efc
gitFedos <yegorf2010@mail.ru>
2016-04-08 09:48:15 +0500

    kkkk

---
+++ 2/2/2.cpp
@@ -0,0 +1,14 @@
+// 2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	std::cout << "Hello world!";
+	std::cin.get();
+	return 0;
+}
+
